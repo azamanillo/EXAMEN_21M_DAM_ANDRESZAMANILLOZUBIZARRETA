e9adf514f97739ae2e4a949dd1d95a03418772a0
zzava <zzava@PC>
2025-05-21 11:43:54 +0200

    Version 1

---
+++ src/primos/NumerosPrimos.java
@@ -0,0 +1,20 @@
+package primos;
+
+import java.util.Scanner;
+
+public class NumerosPrimos {
+	
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);;
+		
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+		
+		int num = teclado.nextInt();
+        teclado.close();
+        
+	}
+
+}
+
